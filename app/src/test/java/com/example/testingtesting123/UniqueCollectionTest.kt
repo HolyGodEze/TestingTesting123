f9645563bb524b4344df74b864ca17284ec09692
Tam Trang <tuo98490@temple.edu>
2024-04-18 12:25:57 -0400

    Created tests for getItem, removeAnItem, removeItemNotInList, and removeItemInEmptyList

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -41,4 +41,48 @@ class UniqueCollectionTest {
 
         assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
     }
+
+    @Test
+    fun getItem(){
+        val item1 = Item("item1")
+        val item2 = Item("item2")
+        collection.addItem(item1)
+        collection.addItem(item2)
+
+        assert(item2 == collection.get(1)) {"Wrong item"}
+
+    }
+
+    @Test
+    fun removeAnItem(){
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+        collection.addItem(Item("item3"))
+
+        val item = Item("item1")
+        collection.remove(item)
+
+        assert(!collection.contains(item)) {"Item in collection"}
+    }
+        @Test
+    fun removeItemNotInList() {
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+        collection.addItem(Item("item3"))
+
+        val item = Item("item4")
+        collection.remove(item)
+
+        assert(!collection.contains(item)) {"Item not removed"}
+    }
+
+    @Test
+    fun removeItemInEmptyList() {
+        val emptyCollection = UniqueCollection()
+
+        val item = Item("item4")
+        emptyCollection.remove(item)
+
+        assert(!emptyCollection.contains(item)) {"Item not removed"}
+    }
 }
